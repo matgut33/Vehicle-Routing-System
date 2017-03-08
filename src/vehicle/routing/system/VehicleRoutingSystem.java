3e0754c76dd7720eae517983baee8c630f252e68
matgut33 <mattgutkin@gmail.com>
2017-03-07 19:15:59 -0500

    Vehicle Routing System

---
+++ src/vehicle/routing/system/VehicleRoutingSystem.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package vehicle.routing.system;
+
+/**
+ * @author Matthew Gutkin, Sam Meyerowitz, Henry Dench
+ * @author Period 8
+ */
+public class VehicleRoutingSystem {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+}
